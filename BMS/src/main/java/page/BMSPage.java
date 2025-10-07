e6d6d4eaa98c3b81bdf6ce97c148bb556c53e0f6
siyadinovatrik <siyad@inovatrik.com>
2025-10-07 16:40:38 +0530

    BMS L1

---
+++ BMS/src/main/java/page/BMSPage.java
@@ -0,0 +1,49 @@
+package page;
+
+import com.microsoft.playwright.Page;
+
+public class BMSPage {
+
+	  public Page page;
+	    
+	    public BMSPage(Page page) {
+	        this.page = page;
+	    }
+	    
+	  public void Verifurl()
+	  {
+		 String URl= page.url();
+		 if(URl.equals("https://bms.mapskil.com/"))
+		 {
+			 System.out.println(URl + "URl verified");
+		 }
+		 else
+		 {
+			 System.out.println("URL not verified");
+		 }
+	  }
+	  public void Veriftittle()
+	  {
+		 String Tittle=page.title();
+		 System.out.println(Tittle);
+		 if(Tittle.equals(Tittle))
+		 {
+			 System.out.println("Tittle verified");
+		 }
+		 else {
+			 System.out.println("Tittle not verified");
+		 }
+	  }
+	  public void booking()
+	  {
+		  
+		  
+		  
+		  
+		  
+		  
+	  }
+}
+
+
+
