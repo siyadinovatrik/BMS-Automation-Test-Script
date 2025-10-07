4bf9701d699930d75ebd57a51b7b31d56b5d1d64
siyadinovatrik <siyad@inovatrik.com>
2025-10-07 16:21:20 +0530

    Created Automation script

---
+++ BMS/src/test/java/test/BMSTest.java
@@ -0,0 +1,41 @@
+package test;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import base.BMSBase;
+import page.BMSPage;
+
+public class BMSTest extends BMSBase
+{
+  BMSPage ob;
+  
+  @BeforeTest
+  public void verifsetup()
+  {
+	  setup();
+	  ob = new BMSPage(page);
+  }
+  @Test(priority=0)
+  public void VeriFURL()
+  {
+	  ob.Verifurl();
+  }
+  @Test(priority=1)
+  public void TittleVeif()
+  {
+	  ob.Veriftittle();
+  }
+  @AfterTest
+  public void teardown()
+  {
+	  page.close();
+	  browser.close();
+	  playwright.close();
+  }
+}
+
+
+
+  
