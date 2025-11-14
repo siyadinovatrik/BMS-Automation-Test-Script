7846ba8b7d211eb40d6f52ae2f25c476c5d22046
siyadinovatrik <siyad@inovatrik.com>
2025-11-14 17:53:14 +0530

    Navigation Flow

--- src/test/java/test/BMSTest.java
+++ src/test/java/test/BMSTest.java
@@ -33,50 +33,55 @@ public class BMSTest extends BMSBase
 	  ob.VerifBrokenLikn();
   }
   @Test(priority=3)
+  public void VerifCheckNavigationFlow() throws Exception
+  {
+	  ob.CheckNavigationFlow();
+  }
+/*  @Test(priority=4)
   public void verifLogin() throws Exception
   {
 	 ob.login(); 
   }
-/* @Test(priority=4)
+  @Test(priority=5)
   public void VerifEditProfile() throws Exception
   {
 	  ob.editProfile();
   }
-  @Test(priority=5)
+  @Test(priority=6)
   public void VerifAddaddress() throws Exception
   {
 	  ob.Addaddress();
   }  
-  @Test(priority=6)
+  @Test(priority=7)
   public void VerifMRIBook() throws Exception
   {
 	  ob.MRIBook();
   }
-  @Test(priority=7)
+  @Test(priority=8)
   public void VerifCTBook() throws Exception
   {
 	  ob.CTbook();
   } 
-  @Test(priority=8)
+  @Test(priority=9)
   public void VerifPETCTBook() throws Exception
   {
 	  ob.PETCTBook();
-  }*/
-  @Test(priority=9)
+  }
+  @Test(priority=10)
   public void VerifEditMobileLogin() throws Exception
   {
 	  ob.EditMobileLogin();
   }
-  @Test(priority=10)
+  @Test(priority=11)
   public void VerifResendOTP() throws Exception
   {
 	  ob.ResendOTP();
   }
-  @Test(priority=11)
+  @Test(priority=12)
   public void VerifInvalidOTP() throws Exception
   {
 	  ob.InvalidOTP();
-  }
+  }*/
   @AfterTest
   public void teardown()
   {
